ab4c88da15b45f1b5ff865bf2a5ee56b0bccf588
agasca <profesorweb@yahoo.com>
2024-01-25 23:45:02 -0600

    Layout

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 console.log("Bienvenido");
 let parrafoSalida = document.querySelector('textarea');
 
-
-
 /*Como utilizar 
-let parrafo = document.getElementsByClassName('.entradaTexto').value;
-parrafoSalida.innerHTML = "Encriptar " + parrafo;
+let e1 = document.getElementsByClassName('entradaTexto').value;
+let e2 = document.getElementById("anuncioTexto").value;
+parrafoSalida.innerHTML = "Encriptar " + parrafo1;
+console.log("class Value "+ e1);
+console.log("class Elemento "+ e2);
+console.dir("class Dir "+ e1);
 */
+
 function encriptar(){
     console.log("Encriptar");
     let parrafo = document.querySelector('.entradaTexto').value;
